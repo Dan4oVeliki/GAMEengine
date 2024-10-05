e5d05f92b674c656ed6a1de84a959c7abc91c7e9
Dan4oVeliki <daani9000@gmail.com>
2024-10-05 17:26:44 +0300

    Created branch and did M1
    Added a few assets(Character controller kit) and used it in a scene

--- Assets/youSpinMeRightRound.cs
+++ Assets/youSpinMeRightRound.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class youSpinMeRightRound : MonoBehaviour
 {
+    public Transform startPosition;
+    public Transform endPosition;
+    public Rigidbody rb;
+    [SerializeField] private Vector3 nextPoint;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,14 @@ public class youSpinMeRightRound : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position == startPosition.position) 
+        {
+            nextPoint = endPosition.position;
+        }
+        if (transform.position == endPosition.position)
+        {
+            nextPoint = startPosition.position;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, nextPoint, Time.deltaTime);
     }
 }
